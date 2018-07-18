32f2017062acde096157dc185d8e22a38fac8e9d
jam <jairoa.marinm>
2018-07-17 20:05:29 -0500

    Ejercicio de adivinar un numero

--- clase1707/pruebaRuby1.rb
+++ clase1707/pruebaRuby1.rb
@@ -7,14 +7,21 @@ name = gets.chomp
 
 puts "Hola \"#{name}\""
 
-puts 'Vamos a sumar dos numero... '
+puts 'Vamos a adivinar un numero... '
 
-puts 'Indique el primer número: '
+puts 'Indique el número: '
 primer = gets.chomp.to_i
-puts 'Indique el segundo número: '
-segundo = gets.chomp.to_i
-puts "La suma de los número es: #{primer+segundo}"
+numero =  Random.new.rand(1..10)
 
-puts 'Hola "mundo'
+print "El número del sistema es: #{numero} "
+if(numero == primer)
+    puts " ADIVINASTE!!!"
+else
+    puts " JAJAAJ Lo siento"
+end
+
+#puts 'Indique el segundo número: '
+#segundo = gets.chomp.to_i
+#puts "La suma de los número es: #{primer+segundo}"
 
 puts "Este es el final del programa"
