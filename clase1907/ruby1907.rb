b2effd80d2592b93b6b178e1d9dd84d37806d731
jam <jairoa.marinm>
2018-07-23 19:57:16 -0500

    Commit inicio de programa practica del lunes 23  de julio

--- clase1907/ruby1907.rb
+++ clase1907/ruby1907.rb
@@ -9,29 +9,35 @@
 #     puts "Su cadena es: #{cadena}, e invertida:  #{cadenaInv} y es no un palindrome" 
 # end
 
-def registrarNumero
-  persona = {}
+# def registrarNumero
+#   persona = {}
 
-  nombre = ""
-  telefono = ""
-    while nombre != 'quit'
+#   nombre = ""
+#   telefono = ""
+#     while nombre != 'quit'
 
-      puts "Por favor indique cual es su nombre, si no desea agregarlo digite 'quit'"
-      nombre = gets.chomp
-      if(nombre != 'quit')
-          puts "Por favor indique cual es su número de telefono"
-          telefono = gets.chomp
-          persona[nombre] = telefono
-          #persona => {nombre: telefono}
-      end
-      # acá va el código que se va a repetir mientras la condición sea verdadera
-    end
+#       puts "Por favor indique cual es su nombre, si no desea agregarlo digite 'quit'"
+#       nombre = gets.chomp
+#       if(nombre != 'quit')
+#           puts "Por favor indique cual es su número de telefono"
+#           telefono = gets.chomp
+#           persona[nombre] = telefono
+#           #persona => {nombre: telefono}
+#       end
+#       # acá va el código que se va a repetir mientras la condición sea verdadera
+#     end
 
-    return persona
+#     return persona
 
-  end
+#   end
 
-puts "Los datos son #{registrarNumero}"
+# puts "Los datos son #{registrarNumero}"
+
+arr = IO.readlines("base.txt")
+#arr = File.read("base.txt")
+mayores10 = []
+
+arr.map { |linea| mayores10.push(linea.chomp) if  linea.to_i > 10 }
 
 
-
+File.write("copia", mayores10)
